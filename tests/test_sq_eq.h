519027082d8e6bec75580dadba0256d93196c823
Rakabidaasta <kvist_tatyana@mail.ru>
2021-03-13 19:14:33 +0300

    Добавлены дополнительные тесты

--- tests/test_sq_eq.h
+++ tests/test_sq_eq.h
@@ -72,5 +72,23 @@ TEST(no_solves, suite3)
     EXPECT_EQ(s.count, 0);
 }
 
+TEST(extra_tests, linear_equation)
+{
+    solves s = solve_equation(0, 1, 2);
+    EXPECT_EQ(s.count, 1);
+    EXPECT_NEAR(s.x1, -2.0, 0.000001);
+}
+
+TEST(extra_tests, zero)
+{
+    solves s = solve_equation(0, 0, 1);
+    EXPECT_EQ(s.count, 0);
+}
+
+TEST(extra_tests, infinity)
+{
+    solves s = solve_equation(0, 0, 0);
+    EXPECT_EQ(s.count, 0);
+}
 
 #endif // TEST_SQ_EQ_H
